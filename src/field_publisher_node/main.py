665570e7f89b29c19c28270199981e8681a7c7f5
Mac #1 <frc195programming@gmail.com>
2023-01-29 21:32:07 +0000

    finished field

--- src/field_publisher_node/main.py
+++ src/field_publisher_node/main.py
@@ -7,6 +7,7 @@ from threading import Thread
 import geometry_msgs.msg
 import field_publisher_node.field_elements as field_elements
 import field_publisher_node.field_elements.constants as constants
+import math
 
 from ck_utilities_py_node.motor import *
 from ck_utilities_py_node.transform_links import *
@@ -21,6 +22,8 @@ def build_community(red_alliance: bool):
     alliance_inverter = 1 if red_alliance else -1
     alliance_color = "red" if red_alliance else "blue"
 
+   
+   
     # Driver Station
     transform = Transform()
     transform.linear.y = 2.74955
@@ -34,7 +37,10 @@ def build_community(red_alliance: bool):
     drive_stations_transform.linear.x = alliance_inverter * 0.0127
     drive_stations_transform.linear.z = 1.9812/2 
     drive_station.set_transform(drive_stations_transform)
-    drive_station.set_color(Color(150.0/255.0, 155.0/255.0, 184.0/255.0, 1.0))
+    if alliance_color == "red":
+        drive_station.set_color(Color(255.0/255.0, 0/255.0, 0/255.0, 1.0))
+    else:
+        drive_station.set_color(Color(0/255.0, 0/255.0, 255.0/255.0, 1.0))
     drive_station.publish()
 
     # First Node Divider
@@ -182,6 +188,155 @@ def build_community(red_alliance: bool):
     charge_station.set_color(Color(205.0/255.0, 205.0/255.0, 205.0/255.0, 0.75))
     charge_station.publish()
 
+    
+    
+
+
+
+    # walls
+    transform = Transform()
+    transform.linear.x = -8.27
+    transform.linear.y = 8.02
+    transform_link = TransformLink("Walls", "map")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+
+    wall = Cube("Walls", 1, "Walls")
+    wall.set_scale(Scale(10.68, 0.0254, 0.51))
+    wall_transform = Transform()
+    wall_transform.linear.z = 0.51/2 
+    wall.set_transform(wall_transform)
+    wall.set_color(Color(150.0/255.0, 155.0/255.0, 184.0/255.0, 1.0))
+    wall.publish()
+
+    # wall side thats longer
+    transform = Transform()
+    transform.linear.x = -8.27
+    transform_link = TransformLink("Walls_2", "map")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+
+    wall_2 = Cube("Walls_2", 2, "Walls_2")
+    wall_2.set_scale(Scale(16.54, 0.0254, 0.51))
+    wall_2_transform = Transform()
+    wall_2_transform.linear.z = 0.51/2 
+    wall_2.set_transform(wall_2_transform)
+    wall_2.set_color(Color(150.0/255.0, 155.0/255.0, 184.0/255.0, 1.0))
+    wall_2.publish()
+
+
+
+    for i in range(0, 2):
+    
+        transform = Transform()
+        transform.linear.y = 5.4991 + 1.22
+        transform.linear.x = alliance_inverter * -0.18 
+        transform_link = TransformLink(f"{alliance_color}double_substation{i}", f"{alliance_color}_map")
+        transform_link.set_transform(transform)
+        transform_link.publish()
+
+        double_substation = Cube(f"{alliance_color}double_substation", i, f"{alliance_color}double_substation{i}")
+        double_substation.set_scale(Scale(alliance_inverter * 0.36, 2.44, 1.98))
+        double_substation_transform = Transform()
+        double_substation_transform.linear.z = 1.98/2 
+        double_substation.set_transform(double_substation_transform)
+        double_substation.set_color(Color(150.0/255.0, 155.0/255.0, 184.0/255.0, 1.0))
+        double_substation.publish()
+    
+    for i in range(0, 2):
+    
+        transform = Transform()
+        transform.linear.y = 5.4991 + 2.44 + 0.36
+        transform.linear.x = alliance_inverter * -1.7
+        transform_link = TransformLink(f"{alliance_color}single_substation{i}", f"{alliance_color}_map")
+        transform_link.set_transform(transform)
+        transform_link.publish()
+
+        single_substation = Cube(f"{alliance_color}single_substation", i, f"{alliance_color}single_substation{i}")
+        single_substation.set_scale(Scale(alliance_inverter * 2.68, 0.69, 2.08))
+        single_substation_transform = Transform()
+        single_substation_transform.linear.z = 2.08/2 
+        single_substation.set_transform(single_substation_transform)
+        single_substation.set_color(Color(150.0/255.0, 155.0/255.0, 184.0/255.0, 1.0))
+        single_substation.publish()
+
+    for i in range(0, 2):
+    
+        transform = Transform()
+        transform.linear.y = 4.707
+        transform.linear.x = alliance_inverter * -7.07
+        transform_link = TransformLink(f"{alliance_color}cubes{i}", f"{alliance_color}_map")
+        transform_link.set_transform(transform)
+        transform_link.publish()
+
+        cubes = Cube(f"{alliance_color}cubes", i, f"{alliance_color}cubes{i}")
+        cubes.set_scale(Scale(alliance_inverter * 0.25, 0.24, 0.24))
+        cubes_transform = Transform()
+        cubes_transform.linear.z = 0.24/2 
+        cubes.set_transform(cubes_transform)
+        cubes.set_color(Color(107.0/255.0, 3.0/255.0, 252.0/255.0, 1.0))
+        cubes.publish()
+    
+    for i in range(0, 2):
+    
+        transform = Transform()
+        transform.linear.y = 0.9327
+        transform.linear.x = alliance_inverter * -7.07
+        transform_link = TransformLink(f"{alliance_color}cubes_2{i}", f"{alliance_color}_map")
+        transform_link.set_transform(transform)
+        transform_link.publish()
+
+        cubes_2 = Cube(f"{alliance_color}cubes_2", i, f"{alliance_color}cubes_2{i}")
+        cubes_2.set_scale(Scale(alliance_inverter * 0.25, 0.24, 0.24))
+        cubes_2_transform = Transform()
+        cubes_2_transform.linear.z = 0.24/2 
+        cubes_2.set_transform(cubes_2_transform)
+        cubes_2.set_color(Color(107.0/255.0, 3.0/255.0, 252.0/255.0, 1.0))
+        cubes_2.publish()
+
+    for i in range(0, 2):
+    
+        transform = Transform()
+        transform.linear.y = 2.267
+        transform.linear.x = alliance_inverter * -7.07
+        transform_link = TransformLink(f"{alliance_color}cones{i}", f"{alliance_color}_map")
+        transform_link.set_transform(transform)
+        transform_link.publish()
+
+        cones = Cylinder(f"{alliance_color}cones", i, f"{alliance_color}cones{i}")
+        cones.set_scale(Scale(alliance_inverter * 0.21, 0.21, 0.33))
+        cones_transform = Transform()
+        cones_transform.linear.z = 0.33/2 
+        cones.set_transform(cones_transform)
+        cones.set_color(Color(252.0/255.0, 186.0/255.0, 3.0/255.0, 1.0))
+        cones.publish()
+    
+     
+    for i in range(0, 2):
+    
+        transform = Transform()
+        transform.linear.y = 3.487
+        transform.linear.x = alliance_inverter * -7.07
+        transform_link = TransformLink(f"{alliance_color}cones_2{i}", f"{alliance_color}_map")
+        transform_link.set_transform(transform)
+        transform_link.publish()
+
+        cones_2 = Cylinder(f"{alliance_color}cones_2", i, f"{alliance_color}cones_2{i}")
+        cones_2.set_scale(Scale(alliance_inverter * 0.21, 0.21, 0.33))
+        cones_2_transform = Transform()
+        cones_2_transform.linear.z = 0.33/2 
+        cones_2.set_transform(cones_2_transform)
+        cones_2.set_color(Color(252.0/255.0, 186.0/255.0, 3.0/255.0, 1.0))
+        cones_2.publish()
+
+
+
+
+
+
+        
+
+
 
 def ros_func():
 
@@ -203,6 +358,8 @@ def ros_func():
 
         build_community(True)
         build_community(False)
+
+        
 
         rate.sleep()
 
